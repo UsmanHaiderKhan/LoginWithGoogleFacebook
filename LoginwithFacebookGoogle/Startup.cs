2b7572a5c3facea527b3fbc568a78d29b2543dfe
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-04-28 17:10:55 +0500

    Add project files.

---
+++ LoginwithFacebookGoogle/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoginwithFacebookGoogle.Startup))]
+namespace LoginwithFacebookGoogle
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
